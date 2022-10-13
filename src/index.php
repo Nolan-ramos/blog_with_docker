519c39fb31d596f5d7d9a998a78da9fdf837eb9b
Nolan-ramos <nolan.ramos.01@gmail.com>
2022-10-13 21:19:50 +0200

    cc

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    // $pdo = new PDO(dsn:"mysql:host=database:3306;dbname=php_db", username:"root", password:"password");
+    echo "coucou theo";
+?>
+</body>
+</html>
